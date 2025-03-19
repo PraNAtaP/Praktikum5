325d850acaa07aac82f94013f5b7c4085edd260b
PraNAtaP <pranatapu08@gmail.com>
2025-03-19 13:02:36 +0700

    Commit 3 - Praktikum 1 InsertionSort

--- Praktikum/SortingMain19.java
+++ Praktikum/SortingMain19.java
@@ -2,13 +2,16 @@ public class SortingMain19 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
+
         Sorting19 dataurut1 = new Sorting19(a, a.length);
         Sorting19 dataurut2 = new Sorting19(b, b.length);
+        Sorting19 dataurut3 = new Sorting19(c, c.length);
 
-        System.out.println("Data awal 2");
-        dataurut2.tampil();
-        dataurut2.SelectionSort();  
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();  
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
-        dataurut2.tampil(); 
+        dataurut3.tampil(); 
     }
 }
